ed501186da360d70e6a2ea3b8938e4585dac1efe
Vlad Balabash <vladb951@gmail.com>
2019-01-27 21:58:52 +0200

    added emails whitelist

--- app/api/binance.js
+++ app/api/binance.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const Router = require('koa-router');
 const R = require('ramda');
 const moment = require('moment');
+const config = require('config');
 
 const queue = require('../../queue');
 
@@ -20,15 +21,33 @@ const symbols = async (ctx) => {
 
 const createJob = async (ctx) => {
     try {
-        let { symbols = [], intervals, startDate, endDate, email } = ctx.request.body;
-        startDate = startDate || '2017-01-01';
-        endDate = endDate || moment().format('YYYY-MM-DD');
-        R.map(symbol => queue.create('binance', { symbol, intervals, startDate, endDate, email }).save())(symbols);
-        ctx.status = 200;
-        ctx.body = {
-            status: 200,
-            message: 'Success'
-        };
+        let {
+            symbols = [], intervals, startDate, endDate, email
+        } = ctx.request.body;
+        const emailsWhitelist = config.get('emailsWhitelist') || [];
+        const isEmailInWhiteList = !!emailsWhitelist.find(e => R.toLower(e) === R.toLower(email));
+        if (isEmailInWhiteList) {
+            startDate = startDate || '2017-01-01';
+            endDate = endDate || moment().format('YYYY-MM-DD');
+            R.map(symbol => queue.create('binance', {
+                symbol,
+                intervals,
+                startDate,
+                endDate,
+                email
+            }).save())(symbols);
+            ctx.status = 200;
+            ctx.body = {
+                status: 200,
+                message: 'Success'
+            };
+        } else {
+            ctx.status = 403;
+            ctx.body = {
+                status: 403,
+                message: 'Permissions denied'
+            };
+        }
     } catch (e) {
         ctx.status = 403;
         ctx.body = 'Error';
