28243b7b0c6622bca50d662e92d26c404a60a9a6
Kirill Museychuk <kmuseychuk@lohika.com>
2019-02-02 22:34:58 +0200

    Refactored endpoints

--- app/api/index.js
+++ app/api/index.js
@@ -10,10 +10,10 @@ const api = new Router({
     prefix: '/api'
 });
 
-api.use('', currencyPairs.routes());
-api.use('', dataSources.routes());
-api.use('', fileExtensions.routes());
-api.use('', intervals.routes());
-api.use('', request.routes());
+api.use(currencyPairs.routes());
+api.use(dataSources.routes());
+api.use(fileExtensions.routes());
+api.use(intervals.routes());
+api.use(request.routes());
 
 module.exports = api;
