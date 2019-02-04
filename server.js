93e09944a78bde2bad06cfcdae9bc6b4ac552b51
Vlad Balabash <vladb951@gmail.com>
2019-02-04 20:17:47 +0200

    added prod env

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const db = require('./app/db');
 const serve = require('koa-static');
 
 db.connect().then(e => {
-    console.log('Connected to mongo');
+  console.log('Connected to mongo');
 });
 
 app.use(serve(`${__dirname}/static`));
@@ -12,10 +12,12 @@ app.use(serve(`${__dirname}/static`));
 app.listen(3000);
 kue.app.listen(3020);
 
-// process.on('uncaughtException', function(error) {
-//     process.exit(1);
-// });
+if (process.env.NODE_ENV === 'production') {
+  process.on('uncaughtException', function (error) {
+    process.exit(1);
+  });
 
-// process.on('unhandledRejection', function(error) {
-//     process.exit(1);
-// });
+  process.on('unhandledRejection', function (error) {
+    process.exit(1);
+  });
+}
