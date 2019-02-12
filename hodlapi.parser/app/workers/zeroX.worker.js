d6e4ab8aad3a8c6b329593fe3140cecc469653aa
Zakrevskijj <lirik1105@gmail.com>
2019-02-12 12:39:48 +0200

    Fix

--- hodlapi.parser/app/workers/zeroX.worker.js
+++ hodlapi.parser/app/workers/zeroX.worker.js
@@ -1,11 +1,12 @@
-const { zeroXParser } = require('../parsers')
-const { ZeroXTransaction } = require('../models')
+const { zeroXParser } = require("../parsers");
+const { ZeroXTransaction } = require("../models");
 
 const zeroXWorker = () => {
-  ZeroXTransaction
-    .countDocuments()
-    .then(zeroXParser)
-    .then((diff) => { console.log(`loaded ${diff} transactions`) })
-}
+    return ZeroXTransaction.countDocuments()
+        .then(zeroXParser)
+        .then(diff => {
+            console.log(`loaded ${diff} transactions`);
+        });
+};
 
-module.exports = { zeroXWorker }
+module.exports = { zeroXWorker };
