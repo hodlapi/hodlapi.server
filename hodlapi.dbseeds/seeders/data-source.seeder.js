5ac3464582d0227050dcf5e99531926cfea24dd4
Vlad Balabash <vladb951@gmail.com>
2019-02-14 23:14:38 +0200

    added shouldRun

--- hodlapi.dbseeds/seeders/data-source.seeder.js
+++ hodlapi.dbseeds/seeders/data-source.seeder.js
@@ -1,11 +1,21 @@
-const { Seeder } = require('mongoose-data-seed');
-const { DataSource } = require('../models');
+const {
+  Seeder
+} = require('mongoose-data-seed');
+const {
+  DataSource
+} = require('../models');
 
 const data = require('../seeds/DataSources.json');
 
 class DataSourceSeeder extends Seeder {
-  
-  run () {
+
+  async shouldRun() {
+    return DataSource.countDocuments()
+      .exec()
+      .then(count => count === 0);
+  }
+
+  run() {
     return DataSource.create(data);
   }
 }
--- hodlapi.dbseeds/seeders/role.seeder.js
+++ hodlapi.dbseeds/seeders/role.seeder.js
@@ -1,9 +1,19 @@
-const { Seeder } = require('mongoose-data-seed');
-const { Role } = require('../models');
+const {
+    Seeder
+} = require('mongoose-data-seed');
+const {
+    Role
+} = require('../models');
 
 const roles = require('../seeds/Roles.json');
 
 class RolesSeeder extends Seeder {
+    async shouldRun() {
+        return Role.countDocuments()
+            .exec()
+            .then(count => count === 0);
+    }
+
     async run() {
         return Role.create(roles);
     }
