db8684c5fbd2ef5edf60f149fe27d611a690a39f
Vlad Balabash <vladb951@gmail.com>
2019-02-15 00:23:42 +0200

    started working on user endpoint

---
+++ hodlapi.core/app/api/user.js
@@ -0,0 +1,13 @@
+const R = require('ramda');
+const router = require('koa-router');
+const { User } = require('../models');
+
+const list = async ctx => {};
+
+const create = async ctx => {};
+
+const remove = async ctx => {};
+
+const update = async ctx => {};
+
+module.exports = {};
