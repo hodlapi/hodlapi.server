2e4e1b8ad5a9b72fc9d5981b3193320f6d7ccfc6
Zakrevskijj <lirik1105@gmail.com>
2019-02-15 13:39:51 +0200

    Initiating binance rates parsing on start

--- hodlapi.core/app/queue.js
+++ hodlapi.core/app/queue.js
@@ -3,23 +3,23 @@ const R = require('ramda');
 const moment = require('moment');
 const config = require('config');
 const {
-  CurrencyPair,
-  getRateByInterval
+    CurrencyPair,
+    getRateByInterval
 } = require('./models');
 const {
-  intervals
+    intervals
 } = require('./lib/constants');
 const {
-  sendEmail,
-  sendSignUpEmail
+    sendEmail,
+    sendSignUpEmail
 } = require('./workers');
 
 const queue = kue.createQueue({
-  redis: {
-    host: config.get('redis.host'),
-    port: config.get('redis.port'),
-    auth: config.get('redis.auth')
-  }
+    redis: {
+        host: config.get('redis.host'),
+        port: config.get('redis.port'),
+        auth: config.get('redis.auth')
+    }
 });
 
 queue.clear();
@@ -27,14 +27,14 @@ queue.clear();
 /******** Jobs createors block ********/
 
 const currencyParsingJob = queue
-  .createJob('parser.binance.currencies', null)
-  .attempts(1)
-  .priority('normal');
+    .createJob('parser.binance.currencies', null)
+    .attempts(1)
+    .priority('normal');
 
 const rateParsingJobs = queue
-  .createJob('core.rateParserStarter', null)
-  .attempts(3)
-  .priority('normal');
+    .createJob('core.rateParserStarter', null)
+    .attempts(3)
+    .priority('normal');
 
 const zeroXParsingJob = queue.createJob('parser.zeroX.transactions', null);
 
@@ -43,6 +43,7 @@ const zeroXParsingJob = queue.createJob('parser.zeroX.transactions', null);
 /******** Scheduler block ********/
 queue.now(currencyParsingJob);
 queue.now(zeroXParsingJob);
+queue.now(rateParsingJobs);
 
 queue.every('1 day', currencyParsingJob);
 queue.every('3 hours', rateParsingJobs);
@@ -53,37 +54,37 @@ queue.every('2 days', zeroXParsingJob);
 /******** Job processors *********/
 
 queue.process('core.rateParserStarter', (_, done) => {
-  intervals.map(({
-    value
-  }) => {
-    CurrencyPair.find()
-      .populate('toId')
-      .populate('fromId')
-      .then(
-        R.map(pair => {
-          getRateByInterval(value)
-            .find({
-              currencyPair: pair._id
-            }, {}, {
-              sort: {
-                openTime: -1
-              }
-            })
-            .then(data => {
-              const start = R.pathOr('2017-01-01', [0, 'openTime'])(data);
-              return queue
-                .create('parser.binance.rates', {
-                  interval: value,
-                  pair,
-                  end: moment(),
-                  start
+    intervals.map(({
+        value
+    }) => {
+        CurrencyPair.find()
+            .populate('toId')
+            .populate('fromId')
+            .then(
+                R.map(pair => {
+                    getRateByInterval(value)
+                        .find({
+                            currencyPair: pair._id
+                        }, {}, {
+                            sort: {
+                                openTime: -1
+                            }
+                        })
+                        .then(data => {
+                            const start = R.pathOr('2017-01-01', [0, 'openTime'])(data);
+                            return queue
+                                .create('parser.binance.rates', {
+                                    interval: value,
+                                    pair,
+                                    end: moment(),
+                                    start
+                                })
+                                .save();
+                        });
                 })
-                .save();
-            });
-        })
-      )
-      .then(done());
-  });
+            )
+            .then(done());
+    });
 });
 
 queue.process('core.sendFileEmail', sendEmail);
