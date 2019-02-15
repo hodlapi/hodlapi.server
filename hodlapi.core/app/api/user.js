2023671d54821891bfb3304841d3249fbd8e9beb
Vlad Balabash <vladb951@gmail.com>
2019-02-15 12:05:20 +0200

    implemented base for users

--- hodlapi.core/app/api/user.js
+++ hodlapi.core/app/api/user.js
@@ -1,7 +1,46 @@
 const R = require('ramda');
-const router = require('koa-router');
-const { User } = require('../models');
+const Router = require('koa-router');
+const {
+  User
+} = require('../models');
+const {
+  aclMiddleware
+} = require('../middlewares');
+const validator = require('koa-joi-validate');
+const joi = require('joi');
 
+const router = new Router();
+
+/**
+ * Validators block
+ */
+
+const listParamsValidator = validator({
+  params: {
+    offset: joi.number(),
+    limit: joi.number()
+  }
+});
+
+const createUserValidator = validator({
+  body: {
+    email: joi.required().email()
+  }
+});
+
+const removeUserValidator = validator({
+  params: {
+    id: joi.number().required()
+  }
+});
+
+const updateUserValidator = validator({
+
+});
+
+/**
+ * Actions block
+ */
 const list = async ctx => {};
 
 const create = async ctx => {};
@@ -10,4 +49,14 @@ const remove = async ctx => {};
 
 const update = async ctx => {};
 
-module.exports = {};
+/**
+ * Routes block
+ */
+router.get('/', aclMiddleware('SUPERADMIN', 'ADMIN'), listParamsValidator, list);
+router.post('/', aclMiddleware('SUPERADMIN', 'ADMIN'), createUserValidator, create);
+router.put('/:id', aclMiddleware('SUPERADMIN', 'ADMIN'), update);
+// TODO: implement redirect to /:id with user.id
+// router.put('/my', );
+router.delete('/', aclMiddleware('SUPERADMIN', 'ADMIN'), removeUserValidator, remove);
+
+module.exports = router;
